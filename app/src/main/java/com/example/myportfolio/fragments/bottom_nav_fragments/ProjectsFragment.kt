036b921b28348659334d1c2a3f8b9785361c8239
Neil <dellosaneil@gmail.com>
2021-01-10 16:13:00 +0800

    created bottom nav bar, nav graph, collapsing tool bar

---
+++ app/src/main/java/com/example/myportfolio/fragments/bottom_nav_fragments/ProjectsFragment.kt
@@ -0,0 +1,21 @@
+package com.example.myportfolio.fragments.bottom_nav_fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myportfolio.R
+
+
+class ProjectsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_projects, container, false)
+
+    }
+}
